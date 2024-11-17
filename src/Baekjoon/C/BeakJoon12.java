77d813908cf86f3943d253ae3746907457d995bd
MoonSeokJun <tjrwns2513@naver.com>
2024-11-17 23:58:47 +0900

    반복문 - A+B - 4

---
+++ src/Baekjoon/C/BeakJoon12.java
@@ -0,0 +1,15 @@
+package Baekjoon.C;
+
+import java.util.Scanner;
+
+public class BeakJoon12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+        sc.close();
+    }
+}
