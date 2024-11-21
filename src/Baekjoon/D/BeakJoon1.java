2c3f5c819335e3bb02aa254ad2924362fa393493
MoonSeokJun <tjrwns2513@naver.com>
2024-11-21 20:43:16 +0900

    위치 변경

--- src/Baekjoon/D/BeakJoon1.java
+++ src/Baekjoon/D/BeakJoon1.java
@@ -1,4 +1,25 @@
 package Baekjoon.D;
 
+import java.util.Scanner;
+
 public class BeakJoon1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int array1[] = new int[n];
+        int count = 0;
+
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] == x)
+                count++;
+        }
+        System.out.println(count);
+        sc.close();
+    }
 }
---
+++ src/Baekjoon/D/BeakJoon3.java
@@ -0,0 +1,7 @@
+package Baekjoon.D;
+
+public class BeakJoon3 {
+    public static void main(String[] args) {
+
+    }
+}
