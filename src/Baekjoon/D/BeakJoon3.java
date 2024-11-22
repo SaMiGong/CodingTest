43420b27014e7a712183b8d4c72b5c60b331f0a4
MoonSeokJun <tjrwns2513@naver.com>
2024-11-22 23:03:55 +0900

    1차원 배열 - 최소, 최대

--- src/Baekjoon/D/BeakJoon3.java
+++ src/Baekjoon/D/BeakJoon3.java
@@ -1,7 +1,21 @@
 package Baekjoon.D;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BeakJoon3 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N - 1]);
     }
 }
